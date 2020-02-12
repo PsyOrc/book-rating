7a4ae6a29baf583cc97ebbb7ea4184561c592156
Sebastian Meine <>
2020-02-12 09:34:43 +0100

    short star variante

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
@@ -6,39 +6,11 @@ import { Book } from '../shared/book';
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss']
 })
-export class BookComponent implements OnInit {
+export class BookComponent {
 
   @Input() book: Book;
-  unFilledStar = '☆';
-  filledStar = '★';
-  starArray = ['☆', '☆', '☆', '☆', '☆'];
-  starRating: string;
 
-  ngOnInit() {
-    this.setStars();
-  }
-
-  setStars() {
-    if (this.book.rating) {
-      for (let rating = 0; rating < this.book.rating; rating++) {
-        this.starArray[rating] = this.filledStar;
-      }
-    }
-    this.starRating = this.starArray.join('');
-  }
-
-  increase() {
-    if (this.book.rating < 5 ) {
-      this.book.rating += 1;
-      this.setStars();
-    }
-  }
-
-  decrease() {
-    if (this.book.rating > 0 ) {
-      this.book.rating -= 1;
-      this.starArray[this.book.rating] = this.unFilledStar;
-      this.setStars();
-    }
+  get Stars() {
+    return new Array(this.book.rating);
   }
 }
