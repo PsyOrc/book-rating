578fe1292a14c68b2bee5f4d1afe16269865bb80
Sebastian Meine <>
2020-02-13 09:35:23 +0100

    UI Test

--- src/app/books/book/book.component.spec.ts
+++ src/app/books/book/book.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import { by, By } from 'protractor';
 
 describe('BookComponent', () => {
   let component: BookComponent;
