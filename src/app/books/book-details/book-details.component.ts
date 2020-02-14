ac54133cf4ac3227a11d8ae040ffe4cc16215943
Sebastian Meine <>
2020-02-14 14:01:46 +0100

    async pipe

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,30 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { of, Observer, timer, Subscription, Observable, concat } from 'rxjs';
-import { map, filter, reduce } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+import { BookStoreService } from 'src/app/book/shared/book-store.service';
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent{
 
-  isbn: string;
-  constructor(private route: ActivatedRoute) { }
-
-  ngOnInit(): void {
-    this.route.paramMap.subscribe(
-      paramMap => this.isbn = paramMap.get('isbn')
+  book$ = this.route.paramMap.pipe(
+    map(paramMap => paramMap.get('isbn')),
+    switchMap(isbn => this.bs.getSingleBook(isbn))
     );
 
-    of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).pipe(
-      map(x => x * 10), // multipliziert x * 10
-      filter(x => x > 30), // filtert alle zahlen unter 30 raus
-      reduce((x,y) => x + y), // summiert das ergebnis
-      map(x => '❤️'.repeat(x))
-    ).subscribe(console.log); // gibt x ❤️ aus
+
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) {
+
+  }
+
+
 
   }
 // http und actiatedRoute muss nicht unsubscribed werden
 
-}
+
